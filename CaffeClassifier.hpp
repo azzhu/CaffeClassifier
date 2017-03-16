b54c9bc41a765aaac2b40666f6a662220216ebae
azzhu <z228812066@vip.qq.com>
2017-03-16 13:28:50 +0800

    Add files via upload

---
+++ CaffeClassifier.hpp
@@ -0,0 +1,68 @@
+///////////////////////////////////
+//  ┏┓　　　┏┓
+// ┏┛┻━━━┛┻┓
+// ┃　　　　　　　┃ 　
+// ┃　　　━　　　┃
+// ┃　┳┛　┗┳　┃
+// ┃　　　　　　　┃
+// ┃　　　┻　　　┃
+// ┃　　　　　　　┃
+// ┗━┓　　　┏━┛
+//     ┃　　　┃
+//     ┃　　　┃
+//     ┃　　　┗━━━┓
+//     ┃　　　　　　　┣┓
+//     ┃　　　　　　　┏┛
+//     ┗┓┓┏━┳┓┏┛
+//       ┃┫┫　┃┫┫
+//       ┗┻┛　┗┻┛
+//
+// Created by ZQJ, 2016.8.24 PM.
+////////////////////////////////////
+
+#define USE_OPENCV
+
+#ifdef USE_OPENCV
+#include <opencv/cv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <f:\caffe-vs2013-master\3rdparty\include\glog\logging.h>
+#endif  // USE_OPENCV
+
+#include <caffe/caffe.hpp>
+
+using namespace std;
+using namespace cv;
+typedef std::pair<string, float> Prediction;
+
+using namespace caffe;  // NOLINT(build/namespaces)
+using std::string;
+
+class Classifier 
+{
+public:
+	Classifier(const string& model_file,
+		const string& trained_file,
+		const string& mean_file,
+		const string& label_file);
+
+	std::vector<Prediction> Classify(const cv::Mat& img, int N = 10);					//显示结果个数
+
+private:
+	void SetMean(const string& mean_file);
+
+	std::vector<float> Predict(const cv::Mat& img);
+
+	void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+
+	void Preprocess(const cv::Mat& img,
+		std::vector<cv::Mat>* input_channels);
+
+private:
+	boost::shared_ptr<Net<float> > net_;
+	cv::Size input_geometry_;
+	int num_channels_;
+	cv::Mat mean_;
+	std::vector<string> labels_;
+};
